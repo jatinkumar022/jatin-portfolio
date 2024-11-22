69be25b23a2ddc9ec5e5e8c48e22aa5aa0b2b10e
Jatinkumar022 <jatinrmn@gmail.com>
2024-11-22 12:33:00 +0530

    Added Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
-
+import Agriculture from "../../Assets/Projects/Agriculture.jpg"
+import Expence from "../../Assets/Projects/Expence.jpg"
+import Nexura from "../../Assets/Projects/Nexura.jpg"
+import Resotan from "../../Assets/Projects/Resotan.jpg"
+import Task from "../../Assets/Projects/Task.jpg"
+import Weather from "../../Assets/Projects/Weather.jpg"
+import BG from "../../Assets/Projects/BG.jpg"
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -23,68 +23,74 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={Agriculture}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Agriculture Marketting"
+              description="Agriculture Marketing is a basic PHP-based platform connecting farmers and traders. It enables easy product listing and inquiries, providing a simple and accessible solution for local agricultural trade."
+              ghLink="https://github.com/jatinkumar022/agriculture-marketing"
+
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={Nexura}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Nexura - Ecommerce"
+              description="Nexura is a React and Firebase-powered e-commerce platform tailored for selling shoes. It offers seamless product browsing, secure transactions, and real-time data management for a smooth shopping experience."
+              ghLink="https://github.com/jatinkumar022/nexura"
+              demoLink="nexura-nine.vercel.app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={Expence}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Finance Insight"
+              description="Expense Tracker is a React and Firebase-based application that helps users manage their finances. It allows users to track income, expenses, and set budgets, with real-time data syncing and secure storage."
+              ghLink="https://github.com/jatinkumar022/FinanceInsight"
+              demoLink="finance-insight-psi.vercel.app"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={Resotan}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Restorn - Restaurant Site"
+              description="Restorn is a restaurant website built with C# and ASP.NET, featuring an online menu, reservations, and order management for a smooth customer experience."
+            // ghLink="https://github.com/soumyajit4419/Plant_AI"
+            // demoLink="https://plant49-ai.herokuapp.com/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={Task}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
+              title="Advanced Task-Manager"
+              description="UAdvanced Task-Manager is a React app for organizing and tracking tasks with features like deadlines and prioritization."
+              ghLink="https://github.com/jatinkumar022/new-task"
+              demoLink="new-task-weld.vercel.app" />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={Weather}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="Weather App"
+              description="Weather is a React app that shows real-time weather updates and forecasts based on user search for a specific location."
+              demoLink="http://rjweather.freewebhostmost.com/"
+            />
+          </Col>  <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={BG}
+              isBlog={false}
+              title="Weather App"
+              description="Weather is a React app that shows real-time weather updates and forecasts based on user search for a specific location."
+              ghLink="https://github.com/jatinkumar022/removebackground"
+              demoLink="https://removebackground-iota.vercel.app/"
             />
           </Col>
         </Row>
